71fc334a33a0f6939d68628fb0c3d73b59d5c54e
Trend20 <enockomondi305@gmail.com>
2021-05-23 05:06:30 +0300

    working on  breeds component

--- src/components/Breeds.js
+++ src/components/Breeds.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import './Main.css';
+
+const apiKey = '73eedd65-45da-425d-849f-f79256229f3e';
+const apiURL = 'https://api.thecatapi.com/v1/breeds';
 
 function Breeds() {
+	const [catBreeds, setCatBreeds] = useState([]);
+
+	useEffect(() => {
+		getCatBreeds();
+	}, []);
+
+	const getCatBreeds = () => {
+		fetch(apiURL, {
+			headers: {
+				'x-api-key': apiKey,
+			},
+		})
+			.then((response) => response.json())
+			.then((catBreeds) => {
+				setCatBreeds(catBreeds);
+				console.log(catBreeds);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	return (
-		<div>
-			<h1>Breeds</h1>
+		<div className="breeds">
+			{catBreeds.map((breed) => {
+				return (
+					<a key={breed.id} href={breed.cfa_url}>
+						<div className="breed-container">
+							<h3>{breed.name}</h3>
+							<h6>id:{breed.id}</h6>
+							<p>{breed.description}</p>
+							<p>{breed.temperament}</p>
+						</div>
+					</a>
+				);
+			})}
 		</div>
 	);
 }
